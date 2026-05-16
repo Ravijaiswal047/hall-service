ecfc32a30e6869bc682cd631c8c2ed4845f3da1f
Ravi Jaiswal <jravi047@gmail.com>
2026-05-16 17:21:45 +0530

    Implement search feature

--- hall-service/src/main/java/com/marriagehall/hall_service/controller/HallController.java
+++ hall-service/src/main/java/com/marriagehall/hall_service/controller/HallController.java
@@ -4,9 +4,9 @@ import com.marriagehall.hall_service.dto.HallRequest;
 import com.marriagehall.hall_service.service.HallService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/halls")
@@ -17,6 +17,21 @@ public class HallController {
 
     public ResponseEntity<?> createHall(@RequestBody HallRequest hallRequest) {
         return ResponseEntity.ok(hallService.createHall(hallRequest));
+
+    }
+
+    public ResponseEntity<?> searchHall(
+            @RequestParam(required = false) String location,
+            @RequestParam(required  = false) Double minPrice,
+            @RequestParam(required = false) Double maxPrice,
+            @RequestParam(required = false) Integer capacity
+            ){
+        return  ResponseEntity.ok(hallService.searchHalls(location, minPrice, maxPrice, capacity));
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<?> getHall(@PathVariable UUID id){
+        return ResponseEntity.ok(hallService.getHallById(id));
 
     }
 
--- hall-service/src/main/java/com/marriagehall/hall_service/service/HallService.java
+++ hall-service/src/main/java/com/marriagehall/hall_service/service/HallService.java
@@ -2,13 +2,20 @@ package com.marriagehall.hall_service.service;
 
 import com.marriagehall.hall_service.dto.HallRequest;
 import com.marriagehall.hall_service.entity.Hall;
+import com.marriagehall.hall_service.repository.HallRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class HallService {
-   public Hall createHall(@RequestBody HallRequest hallRequest) {
+    private final HallRepository hallRepository;
+    public Hall createHall(@RequestBody HallRequest hallRequest) {
        Hall hall = new Hall();
        hall.setName(hallRequest.getName());
        hall.setLocation(hallRequest.getLocation());
@@ -17,5 +24,18 @@ public class HallService {
        hall.setDescription(hallRequest.getDescription());
        hall.setVendorId(hallRequest.getVendorId());
        return hall;
+   }
+
+   public List<Hall> searchHalls(
+           String location,
+           double minPrice,
+           double maxPrice,
+           Integer capacity
+   ){
+       return hallRepository.searchHalls(location, minPrice, maxPrice,capacity);
+   }
+
+   public Hall getHallById(UUID id){
+        return hallRepository.findById(id).orElseThrow(RuntimeException::new);
    }
 }
